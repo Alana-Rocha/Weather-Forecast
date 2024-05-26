84b0cc7cda17354f08219e0d6437a6701c15e8a4
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2024-05-26 19:46:10 -0300

    configuracoes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,36 @@
-import { Box, Flex, Input } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { Box } from "../components/Box";
+import { Input } from "../components/Input";
+import { RiSearch2Line } from "react-icons/ri";
+import { GrLocation } from "react-icons/gr";
 
 // import { useCoordsStore } from "../stores/coords";
 
 export const HomePage = () => {
   // const {
   //   states: { latitude, longitude },
-  // } = useCoordsStore(); 
+  // } = useCoordsStore();
 
   return (
-    <Flex height="100vh" justify="center" align="center">
-      <Box>
-        <Input></Input>
+    <Flex
+      height="100vh"
+      justify="center"
+      align="center"
+      bg="previsao.cinza_imagem"
+    >
+      <Box
+        p="20px"
+        bg="linear-gradient(175deg, rgba(202,77,38,1) 0%, rgba(128,31,0,1) 100%);"
+      >
+        <Flex alignItems="center" gap={1}>
+          <Input label="" placeholder="localização..." p={5} mr={2} />
+          <Flex>
+            <RiSearch2Line size="25px" color="#fff" />
+          </Flex>
+          <Flex>
+            <GrLocation size="25px" color="#fff" />
+          </Flex>
+        </Flex>
       </Box>
     </Flex>
   );
