975258f2175d72be462034bd7ffc13eaf25627e9
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2024-06-17 16:23:05 -0300

    refactor

--- src/utils/localDate.ts
+++
@@ -1,9 +0,0 @@
-// import { DateTime } from "luxon";
-
-// export const formatLocalTime = (dt: number, timezone: string) => {
-//   const localDate = DateTime.fromMillis(dt * 1000, {
-//     zone: "utc",
-//   }).setZone(timezone);
-//   console.log(localDate);
-//   return localDate.toFormat("cccc, dd LLL yyyy | 'Local Time:' HH:mm");
-// };
